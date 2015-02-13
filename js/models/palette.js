1d30fd693bb8a161991a46182b8180bb873df177
Andy Huynh <andy4thehuynh@gmail.com>
2015-02-13 07:50:53 -0800

    ALH: Refactor suggestions logic

--- js/models/palette.js
+++ js/models/palette.js
@@ -12,15 +12,10 @@ Palette = Backbone.Model.extend({
           selectedHair = this.get("hairColor"),
           selectedEyes = this.get("eyeColor");
 
-          console.log(selectedSkin);
-          console.log(selectedHair);
-          console.log(selectedEyes);
-
-
-      var availablePalettes = ['clear-winter', 'cool-winter', 'deep-winter',
-                      'clear-spring', 'warm-spring', 'light-spring',
-                      'light-summer', 'cool-summer', 'soft-summer',
-                      'soft-autumn', 'warm-autumn', 'deep-autumn'];
+      var result = {
+        val: Number.NEGATIVE_INFINITY,
+        keys: []
+      };
 
       var skinMatches = {
         'porcelain':     ['clear-spring', 'deep-autumn', 'warm-spring', 'light-summer', 'light-spring'],
@@ -56,25 +51,18 @@ Palette = Backbone.Model.extend({
         'soft-hazel':    ['soft-summer', 'cool-winter', 'soft-autumn']
       };
 
-      //  Logic here
-      var result = [];
-
       function flattener(arr, selection) {
         for (var i in arr) {
           if (i == selection) {
             for (var j=0; j<arr[i].length; j++) {
-              result.push(arr[i][j]);
+              result.keys.push(arr[i][j]);
             }
           }
         }
       }
 
-      flattener(skinMatches, selectedSkin);
-      flattener(hairMatches, selectedHair);
-      flattener(eyesMatches, selectedEyes);
-
-      function count(ary, classifier) {
-        return ary.reduce(function(counter, item) {
+      function count(obj, classifier) {
+        return obj.keys.reduce(function(counter, item) {
           var p = (classifier || String)(item);
           counter[p] = counter.hasOwnProperty(p) ? counter[p] + 1 : 1;
           return counter;
@@ -82,33 +70,26 @@ Palette = Backbone.Model.extend({
       }
 
       function findPalette(obj) {
-        var max = {
-          val: Number.NEGATIVE_INFINITY,
-          keys: []
-        };
-
         for (var prop in obj) {
           if (obj.hasOwnProperty(prop)) {
             var n = obj[prop];
-            if (n >= max.val) {
-              if (n > max.val) {
-                max.keys = [];
+            if (n >= result.val) {
+              if (n > result.val) {
+                result.keys = [];
               }
-              max.val = n;
-              max.keys.push(prop);
+              result.val = n;
+              result.keys.push(prop);
             }
           }
         }
-        return max;
+        return result;
       }
 
-      var total = count(result);
-      console.log(total);
-      p = findPalette(total);
-      console.log(findPalette(total));
+      flattener(skinMatches, selectedSkin);
+      flattener(hairMatches, selectedHair);
+      flattener(eyesMatches, selectedEyes);
 
-      return findPalette(total);
-      // return 'clear-winter';
+      return findPalette(count(result));
     }
   }
 });
